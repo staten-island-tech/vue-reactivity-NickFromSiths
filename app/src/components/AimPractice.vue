b7dcbef0bbb8bb5f17af361fe044d7eb8ecc8e10
Nick <nmalyarchuk07@gmail.com>
2025-02-06 14:02:32 -0500

    .

--- app/src/components/AimPractice.vue
+++ app/src/components/AimPractice.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="game" @click="(missed++, console.log(randomX, randomY))">
-    <button class="aim" @click="handleClick" :style="{ top: randomY + '%', left: randomX + '%' }">
-      {{ clicked }}
-    </button>
+  <div class="game">
+    <div class="contains">
+      <div class="stats">
+        <div class="stat">Accuracy: {{ ((clicked / (clicked + missed)) * 100).toFixed(1) }}%</div>
+        <div class="stat">Right: {{ clicked }}</div>
+        <div class="stat">Missed: {{ missed }}</div>
+      </div>
+      <div class="offset" @click="(missed++, console.log(randomX, randomY))">
+        <button
+          class="aim"
+          @click="handleClick"
+          :style="{ top: randomY + '%', left: randomX + '%' }"
+        ></button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +23,9 @@ let missed = ref(0)
 let clicked = ref(0)
 let randomX = null
 let randomY = null
-function handleClick() {
+
+function handleClick(event) {
+  event.stopPropagation()
   clicked.value++
   randomX = Math.random() * 100
   randomY = Math.random() * 100
@@ -24,10 +37,29 @@ function handleClick() {
   width: 85%;
   background-color: rgb(186, 255, 232);
 }
+.contains {
+  height: 100%;
+}
+.stats {
+  height: 1.4rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.stat {
+  margin: 0rem 2rem 0rem 2rem;
+}
 .aim {
   position: relative;
-  width: 1.7rem;
-  height: 1.7rem;
+  width: 3rem;
+  height: 3rem;
   border-radius: 100%;
+  z-index: 10;
+}
+.offset {
+  margin: 0.5rem;
+  width: calc(100% - 4rem);
+  height: calc(100% - 5rem);
+  z-index: 1;
 }
 </style>
